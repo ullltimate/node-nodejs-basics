cbddcd04ee693c54d60aa247f8ee30d9bb0d885d
ullltimate <ullltimate.zp@gmail.com>
2023-06-16 14:01:47 +0300

    feat: add solution fs create task

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,17 @@
+import fs from 'fs/promises';
+import path from 'path';
+
 const create = async () => {
-    // Write your code here 
+    const __dirname = path.resolve();
+    try{
+        await fs.appendFile(
+            path.join(__dirname, 'src', 'fs', 'files', 'fresh.txt'),
+            'I am fresh and young', {flag: 'wx'}
+        );
+    } catch(err) {
+        console.error('ERROR:', err);
+        if(err.code === 'EEXIST') throw new Error('FS operation failed');
+    }
 };
 
 await create();
