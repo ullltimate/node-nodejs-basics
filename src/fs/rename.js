546bb5e5ddaee6c876207d20913f2b7ba40ca9e6
ullltimate <ullltimate.zp@gmail.com>
2023-06-18 15:30:13 +0300

    feat: add solution rename task

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,20 @@
+import fs from 'fs/promises';
+import path from 'path';
+
 const rename = async () => {
-    // Write your code here 
+    const __dirname = path.resolve();
+    try {
+        await fs.access(path.join(__dirname, 'src', 'fs', 'files', 'wrongFilename.txt'));
+        const isFile = await fs.access(path.join(__dirname, 'src', 'fs', 'files', 'properFilename.md')).then(()=> true).catch(()=> false);
+        if(!isFile) {
+            await fs.copyFile(path.join(__dirname, 'src', 'fs', 'files', 'wrongFilename.txt'), path.join(__dirname, 'src', 'fs', 'files', 'properFilename.md'));
+            await fs.unlink(path.join(__dirname, 'src', 'fs', 'files', 'wrongFilename.txt'));
+        } else {
+            throw new Error('FS operation failed');
+        }
+    } catch(err) {
+        throw new Error('FS operation failed');
+    }
 };
 
 await rename();
