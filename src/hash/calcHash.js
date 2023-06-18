8ce507733ccc00e8d798c183745c607a63d42a62
ullltimate <ullltimate.zp@gmail.com>
2023-06-18 21:20:19 +0300

    feat: add solution hash task

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,13 @@
+import fs from 'fs/promises';
+import path from 'path';
+import { createHash } from 'crypto';
+
 const calculateHash = async () => {
-    // Write your code here 
+    const __dirname = path.resolve();
+    const hash = createHash('sha256');
+    const data = await fs.readFile(path.join(__dirname, 'src', 'hash', 'files', 'fileToCalculateHashFor.txt'), {encoding: 'utf8'})
+    hash.update(data);
+    console.log(hash.digest('hex'));
 };
 
 await calculateHash();
