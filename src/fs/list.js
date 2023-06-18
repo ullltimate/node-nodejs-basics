4ba870300f1fd21c70ae31fc1a10421825efebb4
ullltimate <ullltimate.zp@gmail.com>
2023-06-18 15:51:09 +0300

    feat: add solution list task

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,14 @@
+import fs from 'fs/promises';
+import path from 'path';
+
 const list = async () => {
-    // Write your code here 
+    const __dirname = path.resolve();
+    try{
+        const files = await fs.readdir(path.join(__dirname, 'src', 'fs', 'files'));
+        console.log(files);
+    } catch {
+        throw new Error('FS operation failed');
+    }
 };
 
 await list();
