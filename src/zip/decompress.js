028c3d13b57c80e68078b5b0a4482f85b185244f
ullltimate <ullltimate.zp@gmail.com>
2023-06-19 13:17:11 +0300

    feat: add solution decompress task

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,18 @@
+import path from 'path';
+import { createReadStream, createWriteStream } from 'fs';
+import { createGunzip } from 'zlib';
+import { pipeline } from 'stream/promises';
+
 const decompress = async () => {
-    // Write your code here 
+    const __dirname = path.resolve();
+    
+    const readStream = createReadStream(path.join(__dirname, 'src', 'zip', 'files', 'archive.gz'));
+    const writeSream = createWriteStream(path.join(__dirname, 'src', 'zip', 'files', 'fileToCompress.txt'));
+    try {
+        await pipeline(readStream, createGunzip(), writeSream,);
+    } catch(err){
+        console.error('ERR: ', err);
+    }
 };
 
 await decompress();
